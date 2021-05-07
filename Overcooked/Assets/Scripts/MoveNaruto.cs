91278445944f991c546e199aa83ef55c558ccaf4
Federico Rubinstein <fede.rubsintein@gmail.com>
2021-05-08 01:37:08 +0200

    Implementadas las animaciones (caminar y correr)

--- Overcooked/Assets/Scripts/MoveNaruto.cs
+++ Overcooked/Assets/Scripts/MoveNaruto.cs
@@ -5,31 +5,42 @@ using UnityEngine;
 public class MoveNaruto : MonoBehaviour
 {
 
-    public float speed = 7.0f;
+    public float speed = 5.0f;
+    public float rotationSpeed = 100.0f;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) // Left move
         {
-            gameObject.transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
+            transform.Rotate(0.0f,  rotationSpeed * Time.deltaTime, 0.0f);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) // Right move
         {
-            gameObject.transform.Translate(+speed * Time.deltaTime, 0.0f, 0.0f);
+            transform.Rotate(0.0f,  -rotationSpeed * Time.deltaTime, 0.0f);
         }
-        else if (Input.GetKey(KeyCode.UpArrow))
-        {
-            gameObject.transform.Translate(0.0f, -speed * Time.deltaTime, 0.0f);
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) // Forward move
+        {   
+            if (Input.GetKey(KeyCode.Space)) // Naruto-run
+            {
+                animator.SetBool("isRunning", true);
+                transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
+            } else {
+                animator.SetBool("isWalking", true);
+                animator.SetBool("isRunning", false);
+                transform.Translate(-speed * Time.deltaTime, 0.0f,  0.0f);
+            }
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            gameObject.transform.Translate(0.0f, +speed * Time.deltaTime, 0.0f);
+        else {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isWalking", false);
         }
     }
 }
