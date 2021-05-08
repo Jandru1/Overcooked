fe9cc3e088751fc5d3b0b3a0b317130ffd7005b9
Federico Rubinstein <fede.rubsintein@gmail.com>
2021-05-08 13:52:31 +0200

    Movimiento casi hecho

--- Overcooked/Assets/Scripts/MoveNaruto.cs
+++ Overcooked/Assets/Scripts/MoveNaruto.cs
@@ -5,42 +5,71 @@ using UnityEngine;
 public class MoveNaruto : MonoBehaviour
 {
 
-    public float speed = 5.0f;
+    public float walkSpeed = 2.0f;
+    public float dashSpeed = 5.0f;
     public float rotationSpeed = 100.0f;
-    Animator animator;
+
+    public ParticleSystem runningDust;
+    private Animator animator;
+    private CharacterController controller;
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 move = new Vector3(0, controller.isGrounded ? 0 : -10, 0);
+
+
         if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) // Left move
         {
-            transform.Rotate(0.0f,  rotationSpeed * Time.deltaTime, 0.0f);
+            controller.transform.Rotate(0.0f,  -rotationSpeed * Time.deltaTime, 0.0f);
+            controller.transform.TransformDirection(0.0f,  -rotationSpeed * Time.deltaTime, 0.0f);
         }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) // Right move
         {
-            transform.Rotate(0.0f,  -rotationSpeed * Time.deltaTime, 0.0f);
+            controller.transform.Rotate(0.0f,  rotationSpeed * Time.deltaTime, 0.0f);
+            controller.transform.TransformDirection(0.0f,  rotationSpeed * Time.deltaTime, 0.0f);
         }
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) // Forward move
         {   
-            if (Input.GetKey(KeyCode.Space)) // Naruto-run
+            if (Input.GetKey(KeyCode.Space) && animator.GetBool("isWalking")) // Naruto-run
             {
+                if(!runningDust.isPlaying)
+                    runningDust.Play();
                 animator.SetBool("isRunning", true);
-                transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
-            } else {
+                //transform.Translate(0.0f, 0.0f, dashSpeed * Time.deltaTime);
+            } else {  // Normal Walk
+                runningDust.Stop();
                 animator.SetBool("isWalking", true);
+                animator.SetBool("isWalkingBack", false);
                 animator.SetBool("isRunning", false);
-                transform.Translate(-speed * Time.deltaTime, 0.0f,  0.0f);
+                move.z += walkSpeed;
+                //transform.Translate(0.0f,  0.0f, walkSpeed * Time.deltaTime);
             }
+        } else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) // Backward move
+        {   
+            runningDust.Stop();
+            animator.SetBool("isWalkingBack", true);
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+            move.z -= walkSpeed;
+            //transform.Translate(0.0f,  0.0f, -walkSpeed * Time.deltaTime);
         }
         else {
+            runningDust.Stop();
             animator.SetBool("isRunning", false);
             animator.SetBool("isWalking", false);
+            animator.SetBool("isWalkingBack", false);
         }
+
+        controller.Move(move * Time.deltaTime);
     }
+
 }
